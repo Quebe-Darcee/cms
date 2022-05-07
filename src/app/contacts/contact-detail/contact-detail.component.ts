51724797c447b9bc25c74a975c7c3f22e6c1964d
Darcee Quebe <darceerl@byui.edu>
2022-05-07 14:10:46 -0600

    Display contact details using event emitters

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Contact } from '../contact.model';
 
@@ -9,7 +9,7 @@ import { Contact } from '../contact.model';
 })
 export class ContactDetailComponent implements OnInit {
 
-  contact: Contact;
+  @Input() contact: Contact;
 
   constructor() { }
 
