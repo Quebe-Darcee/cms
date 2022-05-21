3c3d33c1b1659acb39a5dd7ebcdcc731e85703c4
Darcee Quebe <darceerl@byui.edu>
2022-05-21 15:28:39 -0600

    Create and implement message service

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Message } from '../message.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'cms-message-list',
@@ -8,15 +8,12 @@ import { Message } from '../message.model';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
-  messages: Message[] = [
-    new Message('1', 'Meeting Today', 'Please come to the meeting.', 'Darcee' ),
-    new Message('2', 'How are you?', 'How are you feeling right now?', 'Adam' ),
-    new Message('2', 'Doing good', 'I am doing good. How are you doing?', 'Linda' )
-  ];
+  messages: Message[] = [];
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.messages = this.messageService.getMessages();
   }
 
   onAddMessage(message: Message) {
