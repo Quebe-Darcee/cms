ec29b3802abc2f6b324d57d833149b47931996d1
Darcee Quebe <darceerl@byui.edu>
2022-05-28 21:22:34 -0600

    Implement the view button

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
+import { WindRefService } from '../../wind-ref.service';
 
 @Component({
   selector: 'cms-document-detail',
@@ -11,17 +12,27 @@ import { DocumentService } from '../document.service';
 })
 export class DocumentDetailComponent implements OnInit {
   document: Document;
+  nativeWindow: any;
   id: string;
 
   constructor(private documentService: DocumentService,
+              private windRefService: WindRefService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router) {
+    this.nativeWindow = windRefService.getNativeWindow();
+              }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
       this.document = this.documentService.getDocument(this.id);
     });
+  }
+
+  onView() {
+    if (this.document.url) {
+      this.nativeWindow.open(this.document.url);
+    }
   }
 
 }
