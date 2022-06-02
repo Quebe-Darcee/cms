d025fc3ea2437641f37516017487b6cee064084f
Darcee Quebe <darceerl@byui.edu>
2022-06-01 20:45:08 -0600

    Create add, update and delete contacts functions

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -11,9 +11,49 @@ export class ContactService {
   contactSelectedEvent = new EventEmitter<Contact>();
   contactChangedEvent = new EventEmitter<Contact[]>();
   contactListChangedEvent = new Subject<Contact[]>();
+  maxContactId: number;
 
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
+  }
+
+  getMaxId(): number {
+    var maxId = 0;
+
+    for (let contact of this.contacts) {
+      var currentId = parseInt(contact.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  addContact(newContact: Contact) {
+    if (!newContact) {
+      return
+    }
+    this.maxContactId++;
+    newContact.id = this.maxContactId.toString();
+    this.contacts.push(newContact);
+    var contactsListClone = this.contacts.slice();
+
+    this.contactListChangedEvent.next(contactsListClone);
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact){
+      return
+    }
+    var pos = this.contacts.indexOf(originalContact);
+    if (pos < 0) {
+      return
+    }
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+    var contactsListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactsListClone);
   }
 
   getContacts(): Contact[] {
@@ -38,6 +78,7 @@ export class ContactService {
         return;
      }
      this.contacts.splice(pos, 1);
-     this.contactChangedEvent.emit(this.contacts.slice());
+     var contactsListClone = this.contacts.slice();
+     this.contactListChangedEvent.next(contactsListClone);
   }
 }
