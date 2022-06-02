e7813386ed3134ee46a4e74a6bcfe0a13395e16b
Darcee Quebe <darceerl@byui.edu>
2022-06-01 20:35:20 -0600

    Create add, update, and delete documents functions

--- src/app/documents/document.service.ts
+++ src/app/documents/document.service.ts
@@ -11,9 +11,49 @@ export class DocumentService {
   documentSelectedEvent = new EventEmitter<Document>();
   documentChangedEvent = new EventEmitter<Document[]>();
   documentListChangedEvent = new Subject<Document[]>();
+  maxDocumentId: number;
 
   constructor() {
     this.documents = MOCKDOCUMENTS;
+    this.maxDocumentId = this.getMaxId();
+  }
+
+  getMaxId(): number {
+    var maxId = 0;
+
+    for (let document of this.documents) {
+      var currentId = parseInt(document.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  addDocument(newDocument: Document) {
+    if (!newDocument) {
+      return
+    }
+    this.maxDocumentId++;
+    newDocument.id = this.maxDocumentId.toString();
+    this.documents.push(newDocument);
+    var documentsListClone = this.documents.slice();
+
+    this.documentListChangedEvent.next(documentsListClone);
+  }
+
+  updateDocument(originalDocument: Document, newDocument: Document) {
+    if (!originalDocument || !newDocument){
+      return
+    }
+    var pos = this.documents.indexOf(originalDocument);
+    if (pos < 0) {
+      return
+    }
+    newDocument.id = originalDocument.id;
+    this.documents[pos] = newDocument;
+    var documentsListClone = this.documents.slice();
+    this.documentListChangedEvent.next(documentsListClone);
   }
 
   getDocuments(): Document[] {
@@ -38,6 +78,7 @@ export class DocumentService {
         return;
      }
      this.documents.splice(pos, 1);
-     this.documentChangedEvent.emit(this.documents.slice());
+     var documentsListClone = this.documents.slice();
+     this.documentListChangedEvent.next(documentsListClone);
   }
 }
