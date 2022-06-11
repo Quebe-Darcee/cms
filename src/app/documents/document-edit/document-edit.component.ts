e5aed679adfa89b0a4733309075a7544fe2d87b8
Darcee Quebe <darceerl@byui.edu>
2022-06-10 18:27:26 -0600

    Add document edit form functionality

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
+
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
@@ -10,10 +16,46 @@ export class DocumentEditComponent implements OnInit {
   originalDocument: Document;
   document: Document;
   editMode: boolean = false;
-  
-  constructor() { }
+  private subscription: Subscription;
+
+  constructor(private documentService: DocumentService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      let id = params['id'];
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(id);
+
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    })
   }
 
+onSubmit(form: NgForm) {
+  let value = form.value;
+  console.log(value);
+  let name = value.name;
+  let description = value.description;
+  let url = value.url;
+  let newDocument = new Document('99', name, description, url, null);
+  if (this.editMode === true) {
+    this.documentService.updateDocument(this.originalDocument, newDocument)
+  }
+  else {
+    this.documentService.addDocument(newDocument);
+  }
+  this.router.navigateByUrl('/documents');
+  }
+
+  onCancel() {
+    this.router.navigateByUrl('/documents');
+  }
 }
