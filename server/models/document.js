4715d8d9f1824717c6f6db1421e9ae1fc45d7c3b
Darcee Quebe <darceerl@byui.edu>
2022-07-02 15:26:30 -0600

    Implement documents routing

--- server/models/document.js
+++ server/models/document.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-const documentSchema = mongoose.Schema({
+const documentSchema = mongoose.Schema();
+documentSchema.add({
   id: { type: String, required: true },
   name: { type: String, required: true },
   url: { type: String },
   children: {
     type: [documentSchema],
-    default: undefined }
+    default: undefined
+  }
 });
 
 module.exports = mongoose.model('Document', documentSchema);
